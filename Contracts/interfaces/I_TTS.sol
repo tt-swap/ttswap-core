8bc33ac4a43dc66d383c500a564cff07d10d8ebd
ttswap <ttswap.exchange@gmail.com>
2024-09-07 14:14:30 +0800

    add ttstone
    Signed-off-by: ttswap <ttswap.exchange@gmail.com>

---
+++ Contracts/interfaces/I_TTS.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import {S_ProofKey} from "../libraries/L_Struct.sol";
+import {L_Proof} from "../libraries/L_Proof.sol";
+import {T_BalanceUINT256} from "../libraries/L_BalanceUINT256.sol";
+import {IERC721Permit} from "./IERC721Permit.sol";
+
+/// @title 投资证明接口good's interface
+/// @notice 包含商品的一系列接口  contain good's all interfaces
+interface I_TTS {
+    event e_addreferal(address users, address referal);
+    event e_setenv(
+        uint256 normalgoodid,
+        uint256 valuegoodid,
+        address marketcontract
+    );
+
+    event e_setdaoadmin(address recipent);
+
+    event e_addauths(address auths, uint256 priv);
+
+    event e_rmauths(address auths);
+    event e_addmint(
+        address recipent,
+        uint256 leftamount,
+        uint8 metric,
+        uint8 chips,
+        uint8 index
+    );
+
+    event e_burnmint(uint8 index);
+
+    event e_daomint(uint256 mintamount, uint8 index);
+
+    event e_publicsell(uint256 usdtamount, uint256 ttsamount);
+    event e_synchainstake(
+        uint256 chain,
+        uint256 poolvalue,
+        uint256 proolcontruct,
+        uint256 poolasset
+    );
+    event e_stake(
+        uint256 stakeid,
+        address marketcontract,
+        uint256 proofid,
+        uint256 stakevalue,
+        uint256 stakecontruct
+    );
+
+    event e_unstake(uint256 stakeid, uint256 originid, uint256 profit);
+    event e_updatepool(uint256 poolstate, uint256 stakestate);
+    function getreferal(address _customer) external view returns (address);
+    function addreferal(address user, address referal) external;
+    function stake(address staker, uint128 proofvalue) external;
+    function unstake(
+        address staker,
+        uint128 proofvalue
+    ) external returns (uint128 profit);
+    function isauths(address recipent) external view returns (uint256);
+    function getreferalanddaoamdin(
+        address _customer
+    ) external view returns (address dba_admin, address referal);
+}
---
+++ test/addbanlist.t.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.26;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {MyToken} from "../src/ERC20.sol";
+import "../Contracts/MarketManager.sol";
+import {TTS} from "../Contracts/TTS_Layer1.sol";
+import {BaseSetup} from "./BaseSetup.t.sol";
+import {S_GoodKey, S_ProofKey} from "../Contracts/libraries/L_Struct.sol";
+import {L_ProofIdLibrary, L_Proof} from "../Contracts/libraries/L_Proof.sol";
+import {L_GoodIdLibrary, L_Good} from "../Contracts/libraries/L_Good.sol";
+import {T_BalanceUINT256, toBalanceUINT256} from "../Contracts/libraries/L_BalanceUINT256.sol";
+
+import {L_GoodConfigLibrary} from "../Contracts/libraries/L_GoodConfig.sol";
+import {L_MarketConfigLibrary} from "../Contracts/libraries/L_MarketConfig.sol";
+
+contract addbanlist is BaseSetup {
+    using L_MarketConfigLibrary for uint256;
+    using L_GoodConfigLibrary for uint256;
+    using L_GoodIdLibrary for S_GoodKey;
+    using L_ProofIdLibrary for S_ProofKey;
+
+    uint256 metagood;
+    uint256 normalgoodusdt;
+    uint256 normalgoodbtc;
+
+    function setUp() public override {
+        BaseSetup.setUp();
+    }
+
+    function testaddbanlist() public {
+        vm.startPrank(marketcreator);
+        tts_token.addauths(marketcreator, 2);
+        market.addbanlist(users[4]);
+        assertEq(market.banlist(users[4]), 1, "banlist error");
+
+        market.removebanlist(users[4]);
+        assertEq(market.banlist(users[4]), 0, "banlist error");
+        vm.stopPrank();
+    }
+}
